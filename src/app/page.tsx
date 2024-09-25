eeac31397f550783625d6b85aca59314cfee7a70
gilvanCarvalho <gilvancarvalho@sefaz.pi.gov.br>
2024-09-25 13:52:33 -0300

    implementacao header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Image from 'next/image';
+import Header from './components/Header';
 
 export default function Home() {
   return (
-    <div className='relative h-full lg:h-[140vh]'>
-      <main className='relative pb-24 pl-4 lg:space-y-24 lg:pl-16'>
+    <div className='relative h-screen overflow-hidden bg-gradient-to-b lg:h-[140vh]'>
+      <Header />
+      <main className='relative pb-24 pl-4  lg:pl-16'>
         <div className='flex flex-col space-y-2 py-16 md:space-y-4 lg:h-[65vh] lg:justify-end lg:pb-12'>
         
           <div className='absolute left-0 top-0 -z-10 flex h-[95vh] w-screen flex-col bg-black'>
@@ -22,8 +24,8 @@ export default function Home() {
           </h1>
 
           <p className='text-shadow-md lg:max-w-2x1 max-w-xs text-xs md:max-w-lg md:text-lg'>
-            Geralt of Rivia, a solitary monster hunter, struggles to find his
-            place in a world where people often prove more wicked than beasts.
+            Geralt of Rivia, a solitary monster hunter, struggles to find his place in a world where 
+            people often prove more wicked than beasts.
           </p>
         </div>
 
